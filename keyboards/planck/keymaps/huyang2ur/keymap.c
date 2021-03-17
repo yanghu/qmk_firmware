b7dd5cb8b9af81d926e37a669626d3078995b857
Yang Hu <hueyhy@gmail.com>
2021-03-16 23:12:09 -0700

    add rgblight layer effect

--- keyboards/planck/keymaps/huyang2ur/keymap.c
+++ keyboards/planck/keymaps/huyang2ur/keymap.c
@@ -17,39 +17,65 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
+enum planck_layers {
+    _BASE,
+    _SYMBOL,
+    _NAV,
+    _NUMPAD,
+    _FUNC,
+    _DEBUG,
+    _PSCR,
+    _ENC_SCROLL,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_planck_1x2uR(
-      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
-      LCTL_T(KC_ESC), LCTL_T(KC_A), KC_S, KC_D, LT(7,KC_F), KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
-      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT), 
-      MO(6), KC_LGUI, KC_LALT, MO(7), LT(1,KC_ENT), MO(3), LSFT_T(KC_SPACE), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+  [_BASE] = LAYOUT_planck_1x2uR(
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+      LCTL_T(KC_ESC), LCTL_T(KC_A), KC_S, KC_D, LT(_NUMPAD, KC_F), KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT),
+      TO(_ENC_SCROLL), KC_LGUI, KC_LALT, MO(_NUMPAD), LT(_SYMBOL, KC_ENT), MO(_NAV), LSFT_T(KC_SPACE), KC_LEFT, KC_DOWN, KC_UP, LT(_PSCR, KC_RGHT)),
 
-	[1] = LAYOUT_planck_1x2uR(
-      KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS, 
-      KC_TRNS, KC_GRV, KC_LBRC, KC_LCBR, KC_EQL, KC_PLUS, KC_UNDS, KC_MINS, KC_RCBR, KC_RBRC, KC_COLN, KC_DQUO, 
-      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_PIPE, KC_NO, KC_NO, KC_NO, KC_LT, KC_GT, KC_QUES, KC_TRNS, 
-      BL_STEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MO(4), KC_TRNS, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS),
+	[_SYMBOL] = LAYOUT_planck_1x2uR(
+      KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
+      KC_TRNS, KC_GRV, KC_LBRC, KC_LCBR, KC_EQL, KC_PLUS, KC_UNDS, KC_MINS, KC_RCBR, KC_RBRC, KC_COLN, KC_DQUO,
+      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_PIPE, KC_NO, KC_NO, KC_NO, KC_LT, KC_GT, KC_QUES, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MO(_FUNC), KC_TRNS, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS),
 
-	[3] = LAYOUT_planck_1x2uR(
-      KC_TRNS, KC_NO, LCTL(KC_RGHT), KC_NO, KC_NO, KC_NO, KC_HOME, KC_PGUP, KC_END, KC_NO, KC_NO, KC_NO, 
-      KC_NO, KC_NO, KC_NO, KC_PGDN, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO, 
-      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_LEFT), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, 
-      KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO),
+	[_NAV] = LAYOUT_planck_1x2uR(
+      KC_TRNS, KC_NO, LCTL(KC_RGHT), KC_NO, KC_NO, KC_NO, KC_HOME, KC_PGUP, KC_END, KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_PGDN, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
+      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_LEFT), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
+      KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO),
 
-	[4] = LAYOUT_planck_1x2uR(
-      KC_GRV, RESET, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_TRNS, KC_BSLS, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_TRNS, TO(5), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
+	[_NUMPAD] = LAYOUT_planck_1x2uR(
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NLCK, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PMNS,
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_PAST, KC_PSLS,
+      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_PEQL,
+      KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_P0, KC_P0, KC_DOT, KC_PENT, KC_PENT),
 
-	[5] = LAYOUT_planck_1x2uR(
-      KC_TRNS, RESET, DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, TO(0), BL_TOGG, BL_STEP, BL_BRTG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+	[_FUNC] = LAYOUT_planck_1x2uR(
+      KC_GRV, RESET, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, TO(_DEBUG),
+      KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_TRNS, KC_BSLS, KC_TRNS,
+      KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY),
 
-	[6] = LAYOUT_planck_1x2uR(LCTL(KC_PSCR), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LALT(KC_PSCR), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+	[_DEBUG] = LAYOUT_planck_1x2uR(
+      KC_TRNS, RESET, DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, TO(_BASE),
+      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS, BL_TOGG, BL_STEP, BL_BRTG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-	[7] = LAYOUT_planck_1x2uR(
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NLCK, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PMNS, 
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_PAST, KC_PSLS, 
-      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_PEQL, 
-      KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_P0, KC_P0, KC_DOT, KC_PENT, KC_PENT)
+	[_PSCR] = LAYOUT_planck_1x2uR(
+       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_PSCR),
+       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, LALT(KC_PSCR),
+       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR,
+      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS),
+
+	[_ENC_SCROLL] = LAYOUT_planck_1x2uR(
+      _______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+      _______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+      _______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+      TO(_BASE), _______,_______,_______,_______,_______,_______,_______,_______,_______,_______)
 };
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
@@ -83,4 +109,74 @@ bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
         default:
             return false;
     }
+}
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+   switch(get_highest_layer(layer_state)){
+case _BASE:
+    if (clockwise) {
+      tap_code16(C(KC_RGHT));
+    } else {
+      tap_code16(C(KC_LEFT));
+    }
+   break;
+case _ENC_SCROLL:
+    // Somehow my encoder direction is reversed
+    if (clockwise) {
+        #ifdef MOUSEKEY_ENABLE
+            tap_code(KC_MS_WH_DOWN);
+        #else
+            tap_code(KC_PGDN);
+        #endif
+    }
+    else {
+        #ifdef MOUSEKEY_ENABLE
+            tap_code(KC_MS_WH_UP);
+        #else
+            tap_code(KC_PGUP);
+        #endif
+    }
+   }
+}
+
+
+// RGB lightening
+
+const rgblight_segment_t PROGMEM base_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 9, 0, 0, 0});
+const rgblight_segment_t PROGMEM adjust_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 8, HSV_RED});
+const rgblight_segment_t PROGMEM nav_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 8, HSV_GREEN});
+const rgblight_segment_t PROGMEM numpad_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 8, HSV_BLUE});
+const rgblight_segment_t PROGMEM capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS({4, 5, HSV_WHITE});
+const rgblight_segment_t PROGMEM enc_scroll_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 8, HSV_YELLOW});
+
+// Now define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    base_layer,
+    numpad_layer,
+    nav_layer,
+    enc_scroll_layer,
+    capslock_layer,
+    adjust_layer,
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = rgb_layers;
+    // rgblight_set_layer_state(0, true);
+}
+
+bool led_update_user(led_t led_state) {
+    rgblight_set_layer_state(4, led_state.caps_lock);
+    return true;
+}
+
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // Set RBG layer according to active keymap layer.
+    rgblight_set_layer_state(0, layer_state_cmp(state, _BASE));
+    rgblight_set_layer_state(1, layer_state_cmp(state, _NUMPAD));
+    rgblight_set_layer_state(2, layer_state_cmp(state, _NAV));
+    rgblight_set_layer_state(3, layer_state_cmp(state, _ENC_SCROLL));
+    rgblight_set_layer_state(5, layer_state_cmp(state, _DEBUG));
+    return state;
 }
--- keyboards/planck/keymaps/huyang2ur/rules.mk
+++ keyboards/planck/keymaps/huyang2ur/rules.mk
@@ -1,1 +1,2 @@
 SRC += muse.c
+RGBLIGHT_ENABLE = yes
