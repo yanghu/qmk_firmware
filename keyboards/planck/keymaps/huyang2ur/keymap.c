43319e00d69768346d2e37bd8fb9e1868c1d71d9
Yang Hu <hueyhy@gmail.com>
2021-03-17 23:56:42 -0700

    adjust keymaps

--- keyboards/planck/keymaps/huyang2ur/keymap.c
+++ keyboards/planck/keymaps/huyang2ur/keymap.c
@@ -35,9 +35,9 @@ enum tap_dance_codes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_planck_1x2uR(
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-      LCTL_T(KC_ESC), LCTL_T(KC_A), KC_S, KC_D, LT(_NUMPAD, KC_F), KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+      LCTL_T(KC_ESC), LCTL_T(KC_A), KC_S, KC_D, LT(_NUMPAD, KC_F), KC_G, KC_H, KC_J, LCTL_T(KC_K), KC_L, KC_SCLN, KC_QUOT,
       TD(TD_SFT_CAPS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT),
-      TO(_ENC_SCROLL), KC_LGUI, KC_LALT, MO(_NUMPAD), LT(_SYMBOL, KC_ENT), MO(_NAV), LSFT_T(KC_SPACE), KC_LEFT, KC_DOWN, KC_UP, LT(_PSCR, KC_RGHT)),
+      TO(_ENC_SCROLL), KC_LGUI, KC_LALT, LM(_NUMPAD, MOD_LALT), LT(_SYMBOL, KC_ENT), MO(_NAV), LSFT_T(KC_SPACE), KC_LEFT, KC_DOWN, KC_UP, LT(_PSCR, KC_RGHT)),
 
 	[_SYMBOL] = LAYOUT_planck_1x2uR(
       KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
@@ -47,14 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_NAV] = LAYOUT_planck_1x2uR(
       KC_TRNS, KC_NO, LCTL(KC_RGHT), KC_NO, KC_NO, KC_NO, KC_HOME, KC_PGUP, KC_END, KC_NO, KC_NO, KC_NO,
-      KC_NO, KC_NO, KC_NO, KC_PGDN, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
-      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, LCTL(KC_LEFT), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
+      KC_NO, KC_NO, KC_LSFT, KC_PGDN, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
+      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_LCTL, LCTL(KC_LEFT), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
       KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO),
 
 	[_NUMPAD] = LAYOUT_planck_1x2uR(
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NLCK, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PMNS,
-      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_PAST, KC_PSLS,
-      KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_PEQL,
+      KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_PPLS, KC_PMNS,
+      KC_LCTL, KC_LCTL, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_4, KC_5, KC_6, KC_PAST, KC_PSLS,
+      KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_PEQL, KC_PEQL,
       KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_P0, KC_P0, KC_DOT, KC_PENT, KC_PENT),
 
 	[_FUNC] = LAYOUT_planck_1x2uR(
@@ -105,11 +105,11 @@ bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
 bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LSFT_T(KC_SPACE):
-            return true;
+            return false;
         case LT(1,KC_ENT):
-            return true;
+            return false;
         case LCTL_T(KC_A):
-            return true;
+            return false;
         default:
             return false;
     }
