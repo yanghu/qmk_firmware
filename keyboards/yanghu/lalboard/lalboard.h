ed9c12f350bd110a653d16efa5e337c26a01cfd6
Yang Hu <hueyhy@gmail.com>
2021-06-16 19:03:08 -0700

    add second row to each side

--- keyboards/yanghu/lalboard/lalboard.h
+++ keyboards/yanghu/lalboard/lalboard.h
@@ -30,11 +30,15 @@
 // clang-format off
 #define LAYOUT_lalboard( \
     L00, L01, L02, L03, L04, \
-    R00, R01, R02, R03, R04 \
+    L10, L11, L12, L13, L14, \
+    R00, R01, R02, R03, R04, \
+    R10, R11, R12, R13, R14 \
 ) \
 { \
     { L00, L01, L02, L03, L04 }, \
+    { L10, L11, L12, L13, L14 }, \
     { R00, R01, R02, R03, R04 }, \
+    { R10, R11, R12, R13, R14 }, \
 }
 // clang-format on
 
