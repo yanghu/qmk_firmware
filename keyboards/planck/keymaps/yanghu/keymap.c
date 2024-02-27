09ffc5853b52eeb93c88735587077d29b7256822
Yang Hu <yang@yhu.me>
2024-02-27 10:30:20 -0800

    allow volume control in game layer

--- keyboards/planck/keymaps/yanghu/keymap.c
+++ keyboards/planck/keymaps/yanghu/keymap.c
@@ -121,6 +121,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     case _BASE:
     case _BASE_MAC:
     case _BASE_SIMPLE:
+    case _GAME:
       if (clockwise) {
         /* tap_code16(C(KC_RGHT)); */
         tap_code(KC_VOLU)
